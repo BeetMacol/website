af76b51c5f22cac4d00666603e976cc893e9ae46
BeetMacol <beetmacol@gmail.com>
2021-01-26 21:32:18 +0100

    Failed attempt to add nice visuals for nav items with icons

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,4 @@
-import React, { Component, MouseEvent, MouseEventHandler } from "react";
+import React, { Component, MouseEventHandler } from "react";
 import Head from "next/head";
 import * as lucide from "lucide-react";
 import Link from "next/link";
@@ -20,14 +20,14 @@ export default class Layout extends Component {
 
 				<nav className="bg-secondary text-xl min-w-full py-2 flex container px-4">
 					<div className="w-full my-auto flex space-x-4 flex-row">
-						<LinkNavLi href="/">Home</LinkNavLi>
-						<LinkNavLi href="/type/">Typing</LinkNavLi>
+						<LinkNavItem href="/">Home</LinkNavItem>
+						<LinkNavItem href="/type/">Typing</LinkNavItem>
 					</div>
 					<div className="w-full my-auto flex space-x-4 flex-row-reverse space-x-reverse">
-						<LinkNavLi href="https://github.com/BeetMacol/website" blank={true}><lucide.Github /></LinkNavLi>
-						<NavLi><lucide.Layout /></NavLi>
+						<LinkNavItem href="https://github.com/BeetMacol/website" blank icon><lucide.Github /></LinkNavItem>
+						<NavItem icon><lucide.Layout /></NavItem>
 
-						<NavLi onClick={toggleDropdown}>
+						<NavItem onClick={toggleDropdown} icon>
 							<div className="relative z-10">
 								<lucide.Languages />
 								<DropdownMenu width={36} additionalClasses="-right-3 mt-1 dropdown1">
@@ -35,7 +35,7 @@ export default class Layout extends Component {
 									<DropdownItem>Polish</DropdownItem>
 								</DropdownMenu>
 							</div>
-						</NavLi>
+						</NavItem>
 					</div>
 				</nav>
 
@@ -49,52 +49,54 @@ export default class Layout extends Component {
 	}
 }
 
-class NavLi extends Component<{onClick: MouseEventHandler}> {
+class NavItem extends Component<{ icon: boolean, onClick: MouseEventHandler }> {
 	static defaultProps = {
+		icon: false,
 		onClick: null
 	}
 
 	render() {
-		return (
-			<div className="hover:underline hover:text-select transition cursor-pointer" onClick={this.props.onClick}>
+		return (                                                                           /*   failed attempt of adding a nice visuals for icons :(   */
+			<div className={"hover:underline hover:text-select transition cursor-pointer"/* + (this.props.icon ? " w-8 h-8 rounded-full border" : "")*/} onClick={this.props.onClick}>
 				{this.props.children}
 			</div>
 		)
 	}
 }
 
-class LinkNavLi extends Component<{href: string, blank: boolean}> {
+class LinkNavItem extends Component<{ icon: boolean, href: string, blank: boolean }> {
 	static defaultProps = {
+		icon: false,
 		blank: false
 	}
 
 	render() {
 		return (
-			<NavLi>
-			   <Link href={this.props.href}>
+			<NavItem icon={this.props.icon}>
+				<Link href={this.props.href}>
 					<a target={this.props.blank ? '_blank' : '_self'}>
 						{this.props.children}
 					</a>
 				</Link>
-			</NavLi>
+			</NavItem>
 		)
 	}
 }
 
-class DropdownMenu extends Component<{width: number, additionalClasses: string}, {open: boolean}> {
+class DropdownMenu extends Component<{ width: number, additionalClasses: string }, { open: boolean }> {
 	static defaultProps = {
 		width: 48,
 		additionalClasses: ''
 	}
 
-	constructor(props: {width: number, additionalClasses: string}) {
+	constructor(props: { width: number, additionalClasses: string }) {
 		super(props);
-		this.state = {open: false};
+		this.state = { open: false };
 		languagesDropdown = this; // this will be changed as soon as other dropdown(s) will be added lol
 	}
 
 	setOpen(open: boolean) {
-		this.setState({open: open});
+		this.setState({ open: open });
 		dropdownScreenOverlay.setOpen(open);
 	}
 
@@ -104,7 +106,7 @@ class DropdownMenu extends Component<{width: number, additionalClasses: string},
 
 	render() {
 		return (
-			<div className={"w-" + this.props.width + " bg-tertiary rounded-lg py-2 shadow-md absolute " + this.props.additionalClasses + (this.state.open?"":" hidden")}>
+			<div className={"w-" + this.props.width + " bg-tertiary rounded-lg py-2 shadow-md absolute " + this.props.additionalClasses + (this.state.open ? "" : " hidden")}>
 				{this.props.children}
 			</div>
 		)
@@ -121,15 +123,15 @@ class DropdownItem extends Component {
 	}
 }
 
-class DropdownScreenOverlay extends Component<{}, {open: boolean}> {
+class DropdownScreenOverlay extends Component<{}, { open: boolean }> {
 	constructor(props: {}) {
 		super(props);
-		this.state = {open: false}
+		this.state = { open: false }
 		dropdownScreenOverlay = this;
 	}
 
 	setOpen(open: boolean) {
-		this.setState({open: open});
+		this.setState({ open: open });
 	}
 
 	onClick() {
@@ -139,7 +141,7 @@ class DropdownScreenOverlay extends Component<{}, {open: boolean}> {
 
 	render() {
 		return (
-			<div className={"fixed top-0 right-0 bottom-0 left-0 bg-white opacity-5" + (this.state.open?"":" hidden")} onClick={this.onClick}></div>
+			<div className={"fixed top-0 right-0 bottom-0 left-0 bg-white opacity-5" + (this.state.open ? "" : " hidden")} onClick={this.onClick}></div>
 		)
 	}
 }
